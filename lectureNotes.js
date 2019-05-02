d975d90c4a9df820bb6557046e80137e1c292587
JoshuaHarris93 <josh@emptychairproductions.com>
2019-05-02 16:05:16 +0000

    finding waldos index number

--- lectureNotes.js
+++ lectureNotes.js
@@ -68,6 +68,28 @@ var addTen = function(m) {
 
 var addFive = add(5);
 console.log(addTen(3));
-console.log(addFive(5);)
+console.log(addFive(5));
+
+// create a function that returns a list of numbers divisible by three
+
+var divBy = function(list, fn) {
+    var ourputArr = [];
+    for (var i = 0; i < list.length; i++ ) {
+        if (fn(list[i])) {
+            outputArr.push(list[i]);
+        }
+    }
+    return outputArr;
+}
+
+var numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
+
+var divByThree = function(number) {
+    return number % 3 === 0
+}
+
+console.log(divBy(numbers, divByThree));
+
+//callback function is a function that takes another function as a parameter
 
 
