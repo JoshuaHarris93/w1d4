94c0fd7206157b52f10d96b97db73f9fbc428695
JoshuaHarris93 <josh@emptychairproductions.com>
2019-05-02 16:34:21 +0000

    foreach array example

---
+++ arrayForEach.js
@@ -0,0 +1,16 @@
+function findWaldo(arr, found) {
+    arr.forEach(function(element, i) {
+      if (element === "Waldo") {
+        found(i);   // execute callback
+      }
+    });
+};
+  
+function actionWhenFound(index) {
+    console.log("Found Waldo at index " + index + "!");
+};
+  
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+
